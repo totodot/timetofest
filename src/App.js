5a9ed1bbdc9a3043e2253b8ffed702a55901b9fc
totodot <toto.zetka@gmail.com>
2019-08-19 14:28:48 +0200

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Timetable from './Timetable';
 import config from './config';
 
+const getActiveDayId = (config) => {
+  const current = new Date();
+  const currentTimetable = config.find(({ date }) => {
+    const _date = new Date(date);
+    _date.setDate(_date.getDate() + 1);
+    _date.setHours(8);
+    return current < _date;
+  }) || config[0];
+  return currentTimetable.id;
+}
+
 function App() {
-  const {timetable} = config[0];
+  const [activeDayId, setActiveDatId] = useState(getActiveDayId(config));
+  const { timetable } = config.find(({ id }) => id === activeDayId);
   return (
     <div className="App">
-      <header className="App-header">
-        FEST
-      </header>
+      <div className="days">
+        {config.map(({ id, name }) => (
+          <div key={id} className={`day ${id === activeDayId ? 'day_active' : ''}`} onClick={() => setActiveDatId(id)}>{name}</div>
+        ))}
+      </div>
       <Timetable timetable={timetable} />
 
     </div>
--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default [
     {
         id: 'day1',
+        name: 'day 1',
         date: '2019-08-23',
         timetable: [{
             id: "main",
@@ -36,119 +37,123 @@ export default [
         {
             id: 'tent',
             name: 'Coke TENT',
-            concerts: [{
-                id: 'bemy',
-                name: 'bemy',
-                start: '2019-08-23 15:00',
-                end: '2019-08-23 17:00'
-            }, {
-                id: 'solar',
-                name: 'solar',
-                start: '2019-08-23 17:00',
-                end: '2019-08-23 18:30'
-            }, {
-                id: 'youngigi',
-                name: 'young igi',
-                start: '2019-08-23 18:30',
-                end: '2019-08-23 20:30'
-            }, {
-                id: 'otsochodzi',
-                name: 'otsochodzi',
-                start: '2019-08-23 20:30',
-                end: '2019-08-23 22:00'
-            }, {
-                id: 'nataliaszroeder',
-                name: 'natalia szroeder',
-                start: '2019-08-23 22:00',
-                end: '2019-08-23 23:30'
-            }, {
-                id: 'meute',
-                name: 'meute',
-                start: '2019-08-23 23:30',
-                end: '2019-08-24 01:30'
-            }, {
-                id: 'lostfrequencies',
-                name: 'lost frequencies',
-                start: '2019-08-24 01:30',
-                end: '2019-08-24 03:00',
-                extraInfo: '?'
-            }]
+            concerts:
+                [
+                    {
+                        id: 'bemy',
+                        name: 'bemy',
+                        start: '2019-08-23 15:00',
+                        end: '2019-08-23 17:00'
+                    }, {
+                        id: 'solar',
+                        name: 'solar',
+                        start: '2019-08-23 17:00',
+                        end: '2019-08-23 18:30'
+                    }, {
+                        id: 'youngigi',
+                        name: 'young igi',
+                        start: '2019-08-23 18:30',
+                        end: '2019-08-23 20:30'
+                    }, {
+                        id: 'otsochodzi',
+                        name: 'otsochodzi',
+                        start: '2019-08-23 20:30',
+                        end: '2019-08-23 22:00'
+                    }, {
+                        id: 'nataliaszroeder',
+                        name: 'natalia szroeder',
+                        start: '2019-08-23 22:00',
+                        end: '2019-08-23 23:30'
+                    }, {
+                        id: 'meute',
+                        name: 'meute',
+                        start: '2019-08-23 23:30',
+                        end: '2019-08-24 01:30'
+                    }, {
+                        id: 'lostfrequencies',
+                        name: 'lost frequencies',
+                        start: '2019-08-24 01:30',
+                        end: '2019-08-24 03:00',
+                        extraInfo: '?'
+                    }]
         },
         {
             id: 'silesia',
             name: 'silesia',
-            concerts: [{
-                id: "arekkłusowski",
-                name: "arek kłusowski",
-                start: '2019-08-23 14:00',
-                end: '2019-08-23 15:30'
-            }, {
-                id: "tulia",
-                name: "tulia",
-                start: '2019-08-23 15:30',
-                end: '2019-08-23 17:00'
-            }, {
-                id: 'regeka',
-                name: 'rebeka',
-                start: '2019-08-23 17:00',
-                end: '2019-08-23 19:00'
-            }, {
-                id: "viagraboys",
-                name: "viagra boys",
-                start: '2019-08-23 19:00',
-                end: '2019-08-23 21:00'
-            }, {
-                id: "jmsn",
-                name: "jmsn",
-                start: '2019-08-23 21:00',
-                end: '2019-08-23 23:00'
-            }, {
-                id: "littledragon",
-                name: "little dragon",
-                start: '2019-08-23 23:00',
-                end: '2019-08-24 00:30'
-            }, {
-                id: "christianlöffler",
-                name: "christian löffler",
-                start: '2019-08-24 00:30',
-                end: '2019-08-24 02:00',
-                extraInfo: '?'
-            }]
+            concerts:
+                [{
+                    id: "arekkłusowski",
+                    name: "arek kłusowski",
+                    start: '2019-08-23 14:00',
+                    end: '2019-08-23 15:30'
+                }, {
+                    id: "tulia",
+                    name: "tulia",
+                    start: '2019-08-23 15:30',
+                    end: '2019-08-23 17:00'
+                }, {
+                    id: 'regeka',
+                    name: 'rebeka',
+                    start: '2019-08-23 17:00',
+                    end: '2019-08-23 19:00'
+                }, {
+                    id: "viagraboys",
+                    name: "viagra boys",
+                    start: '2019-08-23 19:00',
+                    end: '2019-08-23 21:00'
+                }, {
+                    id: "jmsn",
+                    name: "jmsn",
+                    start: '2019-08-23 21:00',
+                    end: '2019-08-23 23:00'
+                }, {
+                    id: "littledragon",
+                    name: "little dragon",
+                    start: '2019-08-23 23:00',
+                    end: '2019-08-24 00:30'
+                }, {
+                    id: "christianlöffler",
+                    name: "christian löffler",
+                    start: '2019-08-24 00:30',
+                    end: '2019-08-24 02:00',
+                    extraInfo: '?'
+                }]
         }, {
             id: 'new',
             name: 'new',
-            concerts: [{
-                id: "jagodakret",
-                name: "jagoda kret",
-                start: '2019-08-23 13:30',
-                end: '2019-08-23 14:45'
-            }, {
-                id: "ratkru",
-                name: "rat kru",
-                start: '2019-08-23 14:45',
-                end: '2019-08-23 16:15'
-            }, {
-                id: "bleedingmoses",
-                name: "bleeding moses",
-                start: '2019-08-23 16:15',
-                end: '2019-08-23 18:00'
-            }, {
-                id: "viagraboys",
-                name: "viagra boys",
-                start: '2019-08-23 18:00',
-                end: '2019-08-23 20:00'
-            }, {
-                id: "funkasanki",
-                name: "funkasanki",
-                start: '2019-08-23 20:00',
-                end: '2019-08-23 22:00'
-            }, {
-                id: "meryspolsky",
-                name: "mery spolsky",
-                start: '2019-08-23 22:00',
-                end: '2019-08-24 00:00',
-                extraInfo: '?'
-            }]
+            concerts:
+                [{
+                    id: "jagodakret",
+                    name: "jagoda kret",
+                    start: '2019-08-23 13:30',
+                    end: '2019-08-23 14:45'
+                }, {
+                    id: "ratkru",
+                    name: "rat kru",
+                    start: '2019-08-23 14:45',
+                    end: '2019-08-23 16:15'
+                }, {
+                    id: "bleedingmoses",
+                    name: "bleeding moses",
+                    start: '2019-08-23 16:15',
+                    end: '2019-08-23 18:00'
+                }, {
+                    id: "viagraboys",
+                    name: "viagra boys",
+                    start: '2019-08-23 18:00',
+                    end: '2019-08-23 20:00'
+                }, {
+                    id: "funkasanki",
+                    name: "funkasanki",
+                    start: '2019-08-23 20:00',
+                    end: '2019-08-23 22:00'
+                }, {
+                    id: "meryspolsky",
+                    name: "mery spolsky",
+                    start: '2019-08-23 22:00',
+                    end: '2019-08-24 00:00',
+                    extraInfo: '?'
+                }]
         }, {
             id: 'smolna',
             name: 'smolna',
@@ -259,6 +264,299 @@ export default [
                 name: "wyrx",
                 start: '2019-08-24 04:30',
                 end: '2019-08-24 06:00',
+                extraInfo: '?'
+            }]
+        }]
+    },
+    {
+        id: 'day2',
+        name: 'day 2',
+        date: '2019-08-24',
+        timetable: [{
+            id: "main",
+            name: "main",
+            concerts: [{
+                id: "pawełdomagała",
+                name: "paweł domagała",
+                start: '2019-08-24 16:00',
+                end: '2019-08-24 18:15'
+            }, {
+                id: "dariazawiałow",
+                name: "daria zawiałow",
+                start: '2019-08-24 18:15',
+                end: '2019-08-24 20:30'
+            }, {
+                id: "jadensmith",
+                name: "jaden smith",
+                start: '2019-08-24 20:30',
+                end: '2019-08-24 22:15'
+            }, {
+                id: "metronomy",
+                name: "metronomy",
+                start: '2019-08-24 22:15',
+                end: '2019-08-25 00:15'
+            }, {
+                id: "disclosuredjset",
+                name: "disclosure dj set",
+                start: '2019-08-25 00:15',
+                end: '2019-08-25 2:00',
+                extraInfo: '?'
+            }]
+        },
+        {
+            id: "tent",
+            name: 'Coke TENT',
+            concerts: [{
+                id: 'rau',
+                name: 'rau',
+                start: '2019-08-24 15:00',
+                end: '2019-08-24 16:30'
+            }, {
+                id: 'holak',
+                name: 'holak',
+                start: '2019-08-24 16:30',
+                end: '2019-08-24 18:00'
+            }, {
+                id: 'smolasty',
+                name: 'smolasty',
+                start: '2019-08-24 18:00',
+                end: '2019-08-24 19:30'
+            }, {
+                id: 'bedoes',
+                name: 'bedoes',
+                start: '2019-08-24 19:30',
+                end: '2019-08-24 21:30'
+            }, {
+                id: 'sokół',
+                name: 'sokół',
+                start: '2019-08-24 21:30',
+                end: '2019-08-24 23:30'
+            }, {
+                id: 'netsky',
+                name: 'netsky',
+                start: '2019-08-24 23:30',
+                end: '2019-08-25 02:00'
+            }, {
+                id: 'kamp!',
+                name: 'kamp!',
+                start: '2019-08-25 02:00',
+                end: '2019-08-25 04:00',
+                extraInfo: '?'
+            }]
+        },
+        {
+            id: 'silesia',
+            name: 'silesia',
+            concerts: [{
+                id: "kwiatjabłoni",
+                name: "kwiat jabłoni",
+                start: '2019-08-24 14:00',
+                end: '2019-08-24 15:30'
+            }, {
+                id: "baranovski",
+                name: "baranovski",
+                start: '2019-08-24 15:30',
+                end: '2019-08-24 17:00'
+            }, {
+                id: "kerokerobonito",
+                name: "kero kero bonito",
+                start: '2019-08-24 17:00',
+                end: '2019-08-24 19:00'
+            }, {
+                id: "lionbabe",
+                name: "lion babe",
+                start: '2019-08-24 19:00',
+                end: '2019-08-24 21:00'
+            }, {
+                id: "sevdaliza",
+                name: "sevdaliza",
+                start: '2019-08-24 21:00',
+                end: '2019-08-24 23:00'
+            }, {
+                id: "róisinmurphy",
+                name: "róisin murphy",
+                start: '2019-08-24 23:00',
+                end: '2019-08-25 01:00'
+            }, {
+                id: "rejjiesnow",
+                name: "rejjie snow",
+                start: '2019-08-25 01:00',
+                end: '2019-08-25 03:00',
+                extraInfo: '?'
+            }]
+        }, {
+            id: 'new',
+            name: 'new',
+            concerts: [{
+                id: "atem",
+                name: "atem",
+                start: '2019-08-24 13:30',
+                end: '2019-08-24 15:00'
+            }, {
+                id: "bloocrane",
+                name: "bloo crane",
+                start: '2019-08-24 15:00',
+                end: '2019-08-24 16:15'
+            }, {
+                id: "bluntrazor",
+                name: "blunt razor",
+                start: '2019-08-24 16:15',
+                end: '2019-08-24 18:00'
+            }, {
+                id: "kolory",
+                name: "kolory",
+                start: '2019-08-24 18:00',
+                end: '2019-08-24 20:00'
+            }, {
+                id: "mgły",
+                name: "mgły",
+                start: '2019-08-24 20:00',
+                end: '2019-08-24 22:00'
+            }, {
+                id: "kroki",
+                name: "kroki",
+                start: '2019-08-24 22:00',
+                end: '2019-08-25 00:00',
+                extraInfo: '?'
+            }]
+        }, {
+            id: 'smolna',
+            name: 'smolna',
+            concerts: [{
+                id: "catz’ndogz",
+                name: "catz’n dogz",
+                start: '2019-08-24 20:00',
+                end: '2019-08-24 22:00'
+            }, {
+                id: "oxia",
+                name: "oxia",
+                start: '2019-08-24 22:00',
+                end: '2019-08-25 00:00'
+            }, {
+                id: "vall",
+                name: "vall",
+                start: '2019-08-25 00:00',
+                end: '2019-08-25 02:00'
+            }, {
+                id: "vitalicpres.dima",
+                name: "vitalic pres. dima",
+                extraText: "live",
+                start: '2019-08-25 02:00',
+                end: '2019-08-25 03:00'
+            }, {
+                id: "virginia",
+                name: "virginia",
+                start: '2019-08-25 03:00',
+                end: '2019-08-25 06:00',
+                extraInfo: '?'
+            }]
+        },
+        {
+            id: 'raban',
+            name: 'raban',
+            concerts: [{
+                id: "sinusoydab2binfinity",
+                name: "sinusoyda b2b infinity",
+                extraText: '& mc kriba',
+                start: '2019-08-24 20:00',
+                end: '2019-08-24 21:00'
+            }, {
+                id: "macbeatb2bcontact",
+                name: "macbeat b2b contact ",
+                extraText: '& mc kriba',
+                start: '2019-08-24 21:00',
+                end: '2019-08-24 22:00'
+            }, {
+                id: "alixperezb2bskeptical",
+                name: "alix perez b2b skeptical",
+                extraText: '+ mc mantmast',
+                start: '2019-08-24 22:00',
+                end: '2019-08-25 00:00'
+            }, {
+                id: "ronisize",
+                name: "ronisize",
+                extraText: '+ dynamite mc',
+                start: '2019-08-25 00:00',
+                end: '2019-08-25 01:30'
+            }, {
+                id: "cultureshock",
+                name: "culture shock",
+                start: '2019-08-25 01:30',
+                end: '2019-08-25 03:00',
+            }, {
+                id: "calyx&teebee",
+                name: "calyx & teebee",
+                start: '2019-08-25 03:00',
+                end: '2019-08-25 04:30',
+            }, {
+                id: "matysb2bsick",
+                name: "matys b2b sick",
+                start: '2019-08-25 04:30',
+                end: '2019-08-25 06:00',
+                extraInfo: '?'
+            }]
+        },
+        {
+            id: 'astral',
+            name: 'astral',
+            concerts: [{
+                id: "bartech",
+                name: "bartech",
+                start: '2019-08-24 12:00',
+                end: '2019-08-24 13:45'
+            }, {
+                id: "havaya",
+                name: "havaya",
+                start: '2019-08-24 13:45',
+                end: '2019-08-24 15:30'
+            }, {
+                id: "nois",
+                name: "nois",
+                start: '2019-08-24 15:30',
+                end: '2019-08-24 17:30'
+            }, {
+                id: "ticon",
+                name: "ticon",
+                extraText: "live",
+                start: '2019-08-24 17:30',
+                end: '2019-08-24 19:30'
+            }, {
+                id: "merkaba",
+                name: "merkaba",
+                start: '2019-08-24 19:30',
+                end: '2019-08-24 21:00'
+            }, {
+                id: "melf",
+                name: "melf",
+                start: '2019-08-24 21:00',
+                end: '2019-08-24 22:15',
+            }, {
+                id: "regan",
+                name: "regan",
+                start: '2019-08-24 22:15',
+                end: '2019-08-25 00:15',
+            }, {
+                id: "nozyo",
+                name: "nozyo",
+                start: '2019-08-25 00:15',
+                end: '2019-08-25 01:30'
+            }, {
+                id: "trip",
+                name: "trip",
+                start: '2019-08-25 01:30',
+                end: '2019-08-25 03:00'
+            },
+            {
+                id: "tsuyoshisuzuki",
+                name: "tsuyoshi suzuki",
+                start: '2019-08-25 03:00',
+                end: '2019-08-25 04:30',
+            },
+            {
+                id: "styropian",
+                name: "styropian",
+                start: '2019-08-25 04:30',
+                end: '2019-08-25 06:00',
                 extraInfo: '?'
             }]
         }]
